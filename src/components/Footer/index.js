b5ceae80510ea0ea719b55ce451b759f457419f9
David T. John <davidjohn_atl@yahoo.com>
2020-09-13 15:01:19 -0400

    updated Footer Component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return (
+    <div id="footer-container">
+      <h1>amazin</h1>
+    </div>
+  );
+};
+export default Footer;
