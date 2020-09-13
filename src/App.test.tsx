7b689124aa32ac5e91209beea288819cd104f44a
David T. John <davidjohn_atl@yahoo.com>
2020-09-13 19:25:34 -0400

    Updated styles and app dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Login link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginElement = getByText(/Login/i);
+  expect(loginElement).toBeInTheDocument();
 });
