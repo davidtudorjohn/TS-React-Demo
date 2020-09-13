693ba695cad4d169b97e3e0adbcb85f74b8f0758
David T. John <davidjohn_atl@yahoo.com>
2020-09-13 14:51:59 -0400

    Updated Navbar content

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,16 +3,16 @@ import "./navbar.css";
 const Navbar = () => {
   return (
     <div id="navbar-container">
-      <h1 id="logo">Logo</h1>
+      <h1 id="logo">amazin</h1>
       <ul id="nav-list">
         <li className="nav-item">
-          <a>Link</a>
+          <a>Menu</a>
         </li>
         <li className="nav-item">
-          <a>Link</a>
+          <a>Sign up</a>
         </li>
-        <li className="nav-item  primary-btn">
-          <a>Link</a>
+        <li className="nav-item">
+          <a className="primary-btn">Login</a>
         </li>
       </ul>
     </div>
