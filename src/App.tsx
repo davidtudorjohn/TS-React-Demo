1b674864d6bdbbce9e9fdf3dc5186e518e7bba54
David T. John <davidjohn_atl@yahoo.com>
2020-09-14 10:22:43 -0400

    Updated Navbar and project dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar/index";
 import Footer from "./components/Footer/index";
 import HomePage from "./components/HomePage";
+import Navbar from "./components/Navbar";
+
+// const React = require("react");
+// const Navbar = require("./components/Navbar");
 
 function App() {
   return (
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,39 +1,70 @@
-import React, { Component } from "react";
 import "./navbar.css";
-type NavbarState = {
+import React, { Component } from "react";
+// const React = require("react");
+
+type State = {
   query: string;
 };
-class Navbar extends Component<{}, NavbarState> {
+class Navbar extends Component<{}, State> {
+  tick() {
+    this.setState({
+      query: "",
+    });
+  }
+  componentWillMount() {
+    this.tick();
+  }
+  componentDidMount() {}
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      query: e.target.value,
+    });
+  };
   render() {
     return (
-      <div id="navbar-container">
-        <h1 id="logo">
-          amazin<span id="logoSpan">web</span>
-        </h1>
-        <form id="navbar-search-form">
-          <label id="navbar-search-form-label">
-            <input
-              id="navbar-search-input"
-              type="text"
-              placeholder="Search for anything"
-            />
-            <button id="navbar-search-form-btn">
-              <span className="fa fa-search">Search</span>
-            </button>
-          </label>
-        </form>
-        <ul id="nav-list">
-          <li className="nav-item">
-            <a>Menu</a>
-          </li>
-          <li className="nav-item">
-            <a>Sign up</a>
-          </li>
-          <li className="nav-item">
-            <a className="primary-btn">Login</a>
-          </li>
-        </ul>
-      </div>
+      <>
+        <div id="navbar-container">
+          <h1 id="logo">
+            amazin<span id="logoSpan">web</span>
+          </h1>
+          <form
+            id="navbar-search-form"
+            name="search-form"
+            onSubmit={() => alert("Form Submitted Successfully")}
+          >
+            <label id="navbar-search-form-label">
+              <input
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  this.handleInputChange(e)
+                }
+                id="navbar-search-input"
+                type="text"
+                placeholder="Search for anything"
+              />
+              <button
+                id="navbar-search-form-btn"
+                type="submit"
+                form="search-form"
+              >
+                <span className="fa fa-search">Search</span>
+              </button>
+            </label>
+          </form>
+          <ul id="nav-list">
+            <li className="nav-item">
+              <a href="#">Menu</a>
+            </li>
+            <li className="nav-item">
+              <a href="#">Sign up</a>
+            </li>
+            <li className="nav-item">
+              <a className="primary-btn" href="#">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </>
     );
   }
 }
